d3349305d20325c5334f0e829a880c40ed16eaf4
oanguin <oneal.anguin@ef.com>
2021-12-14 17:33:52 +0000

    add bootstrap support

--- src/Header.elm
+++ src/Header.elm
@@ -7,20 +7,32 @@ import Html.Attributes as Attr
 
 headerLayout : List (Html msg) -> List (Html msg)
 headerLayout children =
-    [ Html.div [ Attr.class "container" ]
-        [ Html.header [ Attr.class "navbar" ]
-            [ viewLink "Home" Route.Home_
-            , viewLink "About Me" Route.AboutMe
+    [ Html.div [ Attr.class "container parent" ]
+        [ Html.nav [ Attr.class "navbar navbar-expand-lg navbar-light bg-white" ]
+            [ Html.div [ Attr.class "container" ]
+                [ Html.a [ Attr.class "navbar-brand", Attr.href "/" ]
+                    [ Html.img [ Attr.src "/images/penguin.png", Attr.alt "Logo", Attr.width 50, Attr.height 40 ] [] ]
+                , Html.ul [ Attr.class "navbar-nav me-auto mb-2 mb-lg-0" ]
+                    [ viewLink "Home" Route.Home_
+                    , viewLink "About Me" Route.AboutMe
+                    ]
+                ]
             ]
         , Html.main_ [] children
         ]
     ]
 
 
+
+-- TODO Make the links look different depending on the page using either class active or something else
+
+
 viewLink : String -> Route -> Html msg
 viewLink title route =
-    Html.a
-        [ Attr.href (Route.toHref route)
-        , Attr.class "menu_item"
+    Html.li [ Attr.class "nav-item" ]
+        [ Html.a
+            [ Attr.href (Route.toHref route)
+            , Attr.class "menu_item nav-link active"
+            ]
+            [ Html.text title ]
         ]
-        [ Html.text title ]
